d7daf678c8f782ef5df35994c26540754e180161
YashaShyro <abizard.zufar@gmail.com>
2025-03-26 15:18:50 +0700

    second try commit

---
+++ per3/tugas2/Calc.js
@@ -0,0 +1,43 @@
+const kalkulator = {
+    tambah: (...angka) => {
+      let hasil = 0;
+      for (let i = 0; i < angka.length; i++) {
+        hasil += angka[i];
+      }
+      return hasil;
+    },
+  
+    kurang: (...angka) => {
+      let hasil = angka[0];
+      for (let i = 1; i < angka.length; i++) {
+        hasil -= angka[i];
+      }
+      return hasil;
+    },
+  
+    kali: (...angka) => {
+      let hasil = 1;
+      for (let i = 0; i < angka.length; i++) {
+        hasil *= angka[i];
+      }
+      return hasil;
+    },
+  
+    bagi: (...angka) => {
+      let hasil = angka[0];
+      for (let i = 1; i < angka.length; i++) {
+        hasil /= angka[i];
+      }
+      return hasil;
+    },
+  
+    modulus: (...angka) => {
+      let hasil = angka[0];
+      for (let i = 1; i < angka.length; i++) {
+        hasil %= angka[i];
+      }
+      return hasil;
+    },
+  };
+  
+  console.log("Penjumlahan: " + kalkulator.tambah(10, 5, 2));
